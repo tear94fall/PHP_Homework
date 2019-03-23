2568b48eafda5eaa15c8762e118921b06201e582
tear94fall <tear94fall@naver.com>
2019-03-23 11:29:30 +0900

    글 작성 작동 되도록 수정

--- PHPweb2019/notice/insert.php
+++ PHPweb2019/notice/insert.php
@@ -1,28 +1,33 @@
-<?
-   session_start();
+<?php
+session_start();
+$subject = $_REQUEST['subject'];
+$content = $_REQUEST['content'];
 
-   if(!$subject) {
-     echo("<script>window.alert('제목을 입력하세요.'); history.go(-1)</script>");
-	 exit;
-   }
-   
-   if(!$content) {
-     echo("<script>window.alert('내용을 입력하세요.'); history.go(-1)</script>");
-	 exit;
-   }
-   
-   include "../dbconn.php";       // dconn.php 파일을 불러옴
+$userid = $_SESSION['userid'];
+$username = $_SESSION['username'];
 
-   $regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
+if(!$subject) {
+    echo("<script>window.alert('제목을 입력하세요.'); history.go(-1)</script>");
+    exit;
+}
+
+if(!$content) {
+    echo("<script>window.alert('내용을 입력하세요.'); history.go(-1)</script>");
+    exit;
+}
+
+include "../dbconn.php";       // dconn.php 파일을 불러옴
+
+$regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
 $ip = $_SERVER["REMOTE_ADDR"];        // 방문자의 IP 주소를 저장
 
-   // 레코드 삽입 명령
-   $sql = "insert into notice_board(id, name, subject, content, regist_day, hit, ip) ";
-   $sql .= "values('$userid', '$username', '$subject', '$content', '$regist_day', 0, '$ip')";    
-   
-   mysql_query($sql, $connect);  // $sql 에 저장된 명령 실행
+// 레코드 삽입 명령
+$sql = "insert into notice_board(id, name, subject, content, regist_day, hit, ip) ";
+$sql .= "values('$userid', '$username', '$subject', '$content', '$regist_day', 0, '$ip')";
 
-   mysql_close();                // DB 연결 끊기
-   
-   Header("Location:list.php");  // list.php 로 이동합니다.
+$result = $connect->query($sql) or die($this->_connect->error);
+
+$connect->close();            // DB 연결 끊기
+
+Header("Location:list.php");  // list.php 로 이동합니다.
 ?>
