cfb2797e1da83f97d07b369abfd4099d83be0f9f
tear94fall <tear94fall@naver.com>
2019-03-23 11:28:11 +0900

    이름이 표시되도록 수정함

--- PHPweb2019/notice/write_form.php
+++ PHPweb2019/notice/write_form.php
@@ -1,91 +1,83 @@
-<?
-   session_start();
+<?php
+session_start();
+$userid = $_SESSION['userid'];
+$username = $_SESSION['username'];
 
-   if ($userid != "admin")
-   {
-     echo("
-           <script>
-             window.alert('∞¸∏Æ¿⁄∏∏ ±€¿ª æµ ºˆ ¿÷Ω¿¥œ¥Ÿ.')
-             history.go(-1)
-           </script>
-         ");
-         exit;
-   }
-   else
-   {
+if ($userid != "admin")
+{
+    echo("<script>window.alert('Í¥ÄÎ¶¨ÏûêÎßå Í∏ÄÏùÑ Ïì∏ Ïàò ÏûàÏäµÎãàÎã§.'); history.go(-1)</script>");
+    exit;
+}else{
+}
 ?>
 <html>
- <head>
-  <title>:: PHP «¡∑Œ±◊∑°π÷ ¿‘πÆø° ø¿Ω≈∞Õ¿ª »Øøµ«’¥œ¥Ÿ~~ ::</title>
- <link rel='stylesheet' href='../style.css' type='text/css'>	
- </head>
- <body leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>
+<head>
+    <title>:: PHP ÌîÑÎ°úÍ∑∏ÎûòÎ∞ç ÏûÖÎ¨∏Ïóê Ïò§Ïã†Í≤ÉÏùÑ ÌôòÏòÅÌï©ÎãàÎã§~~ ::</title>
+    <link rel='stylesheet' href='../style.css' type='text/css'>
+</head>
+<body leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>
 
-    <table width=776 align=center border=0 cellspacing=0 cellpadding=0>
-       <form name='writeform' action='insert.php' method='post'>
+<table width=776 align=center border=0 cellspacing=0 cellpadding=0>
+    <form name='writeform' action='insert.php' method='post'>
         <tr><td colspan='6' height=25><img src='img/notice_title.gif'></td></tr>
         <tr><td background='img/bbs_bg.gif'>
-            <img border='0' src='img/blank.gif' width='1' height='3'></td></tr>
+                <img border='0' src='img/blank.gif' width='1' height='3'></td></tr>
         <tr><td height=10></td></tr>
 
-          <td align=center colspan=2>
-    <table width=776 border=0 cellspacing=0 cellpadding=0 class='txt' 
-         bgcolor=#F7F7F2>
-        <tr height=1 bgcolor=#5AB2C8><td></td></tr>
-        <tr height=1 bgcolor=#5AB2C8><td></td></tr>
-        <tr>
-          <td>
-    <table width='100%' border=0 cellspacing=0 cellpadding=0 class='txt'>
-        <tr height=25>
-          <td align=right width=100>¿Ã∏ß&nbsp;</td>
-          <td align=left> : <? echo $username ?> </td>
-        </tr>
-     </table>
-          </td>
-        </tr>
-        <tr height=1 bgcolor=#5AB2C8><td colspan=2></td></tr>
-        <tr bgcolor='#D2EAF0' height=20>
-          <td colspan=2>&nbsp;&nbsp;<b>«œ∞Ì ΩÕ¿∫ ∏ª¿ª ≥≤±‚ººø‰.</b></td>
-        </tr>        
-        <tr height=1 bgcolor=#5AB2C8><td colspan=5></td></tr>  
-        <tr>
-          <td colspan=2>
-    <table width='100%' border=0 cellspacing=0 cellpadding=0 class='txt'>
-        <tr> 
-          <td height=25>&nbsp;&nbsp;¡¶∏Ò&nbsp;&nbsp;&nbsp;
-             <input style='font-size:9pt;border:1px solid' type='text' 
-                     name='subject' size=50 maxlength=100></td>
-        </tr>
-     
-        <tr valign=top>
-          <td align='center' >
-             <p align='left'>&nbsp; ≥ªøÎ&nbsp;&nbsp;&nbsp;
-             <textarea style='font-size:9pt;border:1px solid' name='content' 
-                       style=background-image:url('img/bbs_text_line.gif'); 
-                        cols=74 rows=12 wrap=virtual></textarea></td>
-        </tr>
-        <tr height=20>
-          <td></td>
-        </tr>
-        <tr height=1 bgcolor=#5AB2C8><td></td></tr>
-        <tr>
-          <td height=30 align=center valign=top bgcolor='FFFFFF'><br>
+        <td align=center colspan=2>
+            <table width=776 border=0 cellspacing=0 cellpadding=0 class='txt'
+                   bgcolor=#F7F7F2>
+                <tr height=1 bgcolor=#5AB2C8><td></td></tr>
+                <tr height=1 bgcolor=#5AB2C8><td></td></tr>
+                <tr>
+                    <td>
+                        <table width='100%' border=0 cellspacing=0 cellpadding=0 class='txt'>
+                            <tr height=25>
+                                <td align=right width=100>Ïù¥Î¶Ñ&nbsp;</td>
+                                <td align=left> : <?php echo $username ?> </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr height=1 bgcolor=#5AB2C8><td colspan=2></td></tr>
+                <tr bgcolor='#D2EAF0' height=20>
+                    <td colspan=2>&nbsp;&nbsp;<b>ÌïòÍ≥† Ïã∂ÏùÄ ÎßêÏùÑ ÎÇ®Í∏∞ÏÑ∏Ïöî.</b></td>
+                </tr>
+                <tr height=1 bgcolor=#5AB2C8><td colspan=5></td></tr>
+                <tr>
+                    <td colspan=2>
+                        <table width='100%' border=0 cellspacing=0 cellpadding=0 class='txt'>
+                            <tr>
+                                <td height=25>&nbsp;&nbsp;Ï†úÎ™©&nbsp;&nbsp;&nbsp;
+                                    <input style='font-size:9pt;border:1px solid' type='text'
+                                           name='subject' size=50 maxlength=100></td>
+                            </tr>
 
-           <input type=image src='img/i_write.gif' align=absmiddle border=0>
-              &nbsp;<a href='list.php'><img style='cursor:hand' 
-                     src='img/i_list.gif' align=absmiddle border=0 ></a></td>
+                            <tr valign=top>
+                                <td align='center' >
+                                    <p align='left'>&nbsp; ÎÇ¥Ïö©&nbsp;&nbsp;&nbsp;
+                                        <textarea style='font-size:9pt;border:1px solid' name='content'
+                                                  style=background-image:url('img/bbs_text_line.gif');
+                                                  cols=74 rows=12 wrap=virtual></textarea></td>
+                            </tr>
+                            <tr height=20>
+                                <td></td>
+                            </tr>
+                            <tr height=1 bgcolor=#5AB2C8><td></td></tr>
+                            <tr>
+                                <td height=30 align=center valign=top bgcolor='FFFFFF'><br>
+
+                                    <input type=image src='img/i_write.gif' align=absmiddle border=0>
+                                    &nbsp;<a href='list.php'><img style='cursor:hand'
+                                                                  src='img/i_list.gif' align=absmiddle border=0 ></a></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
         </tr>
-    </table>
-          </td>
-        </tr> 
-    </table>
-          </td>
-        </tr>
-       </form>
-    </table>
-  </body>
+    </form>
+</table>
+</body>
 </html>
-
-<?
-    }
-?>
