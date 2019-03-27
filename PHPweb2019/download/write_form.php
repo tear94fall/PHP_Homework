9ceaf9280448ec9db1102bd9483540c2fcc8ad26
tear94fall <tear94fall@naver.com>
2019-03-27 09:31:56 +0900

    첨부파일 게시판 글쓰기 폼 ui정상적으로 작동 로직은 아직 수정중

--- PHPweb2019/download/write_form.php
+++ PHPweb2019/download/write_form.php
@@ -1,120 +1,126 @@
-<?
-   session_start();
+<?php
+session_start();
 
-   if (!$userid)
-   {
-      echo("
+$userid = $_SESSION['userid'];
+$username = $_SESSION['username'];
+
+$page = $_REQUEST['page'];
+$num = $_REQUEST['num'];
+
+if (!$userid)
+{
+    echo("
 	   <script>
-             window.alert('∑Œ±◊¿Œ »ƒ ±€æ≤±‚∏¶ «œº≈æﬂ «’¥œ¥Ÿ.')
+             window.alert('Î°úÍ∑∏Ïù∏ ÌõÑ Í∏ÄÏì∞Í∏∞Î•º ÌïòÏÖîÏïº Ìï©ÎãàÎã§.')
              history.go(-1)
            </script>
            ");
-      exit;
-   }
+    exit;
+}
 ?>
 <html>
- <head>
-  <title>:: PHP «¡∑Œ±◊∑°π÷ ¿‘πÆø° ø¿Ω≈ ∞Õ¿ª »Øøµ«’¥œ¥Ÿ~~ ::</title>
-  <link rel='stylesheet' href='../style.css' type='text/css'>	
- </head>
- <body leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>
-     <form name='writeform' 
-         action='insert.php?num=<? echo $num ?>&page=<? echo $page ?>' 
-         method='post' enctype='multipart/form-data'>
+<head>
+    <title>:: PHP ÌîÑÎ°úÍ∑∏ÎûòÎ∞ç ÏûÖÎ¨∏Ïóê Ïò§Ïã† Í≤ÉÏùÑ ÌôòÏòÅÌï©ÎãàÎã§~~ ::</title>
+    <link rel='stylesheet' href='../style.css' type='text/css'>
+</head>
+<body leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>
+<form name='writeform'
+      action='insert.php?num=<?php echo $num ?>&page=<?php echo $page ?>'
+      method='post' enctype='multipart/form-data'>
     <table width=776 border=0 cellspacing=0 cellpadding=0 align=center>
         <tr>
-          <td colspan="5" height=25><img src="img/down_title.gif"></td>
+            <td colspan="5" height=25><img src="img/down_title.gif"></td>
         </tr>
         <tr>
-          <td background="img/bbs_bg.gif">
-             <img border="0" src="img/blank.gif" 
-                   width="1" height="3"></td>
+            <td background="img/bbs_bg.gif">
+                <img border="0" src="img/blank.gif"
+                     width="1" height="3"></td>
         </tr>
         <tr>
-          <td height=10></td>
+            <td height=10></td>
         </tr>
-          <td align=center colspan=2>
-    <table width=776 border=0 cellspacing=0 cellpadding=0 class='txt'
-         bgcolor=#F7F7F2>
-        <tr height=1 bgcolor=#5AB2C8><td></td></tr>
-        <tr height=1 bgcolor=#5AB2C8><td></td></tr>
-        <tr>
-          <td>
-    <table width='100%' border=0 cellspacing=0 cellpadding=0 class='txt'>
-        <tr height=25>
-          <td align=right width=100>¿Ã∏ß&nbsp;</td>
-          <td align=left> : <? echo $username ?> </td>
-        </tr>
-    </table>
-          </td>
-        </tr>
-        <tr height=1 bgcolor=#5AB2C8><td colspan=2></td></tr>
-        <tr bgcolor='#D2EAF0' height=20>
-          <td colspan=2>&nbsp;&nbsp;<b>«œ∞Ì ΩÕ¿∫ ∏ª¿ª ≥≤±‚ººø‰.</b></td>
-        </tr>        
-        <tr height=1 bgcolor=#5AB2C8><td colspan=5></td></tr>  
-        <tr>
-           <td colspan=2>
-    <table width='100%' border=0 cellspacing=0 cellpadding=0 class='txt'>
-<?
-   if ($num)
-   {
-      include "../dbconn.php";
-    
-      $sql = "select * from down_board where num = $num";
-      $result = mysql_query($sql, $connect);  
-      $row = mysql_fetch_array($result);
+        <td align=center colspan=2>
+            <table width=776 border=0 cellspacing=0 cellpadding=0 class='txt'
+                   bgcolor=#F7F7F2>
+                <tr height=1 bgcolor=#5AB2C8><td></td></tr>
+                <tr height=1 bgcolor=#5AB2C8><td></td></tr>
+                <tr>
+                    <td>
+                        <table width='100%' border=0 cellspacing=0 cellpadding=0 class='txt'>
+                            <tr height=25>
+                                <td align=right width=100>Ïù¥Î¶Ñ&nbsp;</td>
+                                <td align=left> : <?php echo $username ?> </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr height=1 bgcolor=#5AB2C8><td colspan=2></td></tr>
+                <tr bgcolor='#D2EAF0' height=20>
+                    <td colspan=2><b>ÌïòÍ≥† Ïã∂ÏùÄ ÎßêÏùÑ ÎÇ®Í∏∞ÏÑ∏Ïöî.</b></td>
+                </tr>
+                <tr height=1 bgcolor=#5AB2C8><td colspan=5></td></tr>
+                <tr>
+                    <td colspan=2>
+                        <table width='100%' border=0 cellspacing=0 cellpadding=0 class='txt'>
+                            <?php
+                            if ($num)
+                            {
+                                include "../dbconn.php";
 
-      $subject = "[re]".$row[subject];
-      $content = ">".$row[content];
-      $content = str_replace("\n", "\n>", $content);
-      $content = "\n\n".$content;
+                                $sql = "select * from down_board where num = $num";
+                                $result = $connect->query($sql) or die($this->_connect->error);
+                                $row = $result->fetch_array();
 
-      mysql_close();
-   }
-?>
-        <tr height=10><td colspan=2></td> </tr>
-        <tr> 
-          <td width=70 height=25 align=left>&nbsp;&nbsp;¡¶∏Ò
-          </td>
-          <td height=25 >
-           <input style='font-size:9pt;border:1px solid' type='text' 
-                  name='subject' size=50 maxlength=100 
-                 value='<? echo $subject ?>'></td>
-        </tr>
-     
-        <tr valign=top>
-          <td width=70 height=25 align=left>&nbsp;&nbsp;≥ªøÎ
-          </td>
-          <td>
-            <textarea style='font-size:9pt;border:1px solid'name='content' 
-                      style=background-image:url('img/bbs_text_line.gif'); 
-                      cols=74 rows=12 wrap=virtual><? echo $content ?>
+                                $subject = "[re]".$row[subject];
+                                $content = ">".$row[content];
+                                $content = str_replace("\n", "\n>", $content);
+                                $content = "\n\n".$content;
+
+                                $connect->close();
+                            }
+                            ?>
+                            <tr height=10><td colspan=2></td> </tr>
+                            <tr>
+                                <td width=70 height=25 align=left>&nbsp;&nbsp;Ï†úÎ™©
+                                </td>
+                                <td height=25 >
+                                    <input style='font-size:9pt;border:1px solid' type='text'
+                                           name='subject' size=50 maxlength=100
+                                           value='<?php echo $subject ?>'></td>
+                            </tr>
+
+                            <tr valign=top>
+                                <td width=70 height=25 align=left>&nbsp;&nbsp;ÎÇ¥Ïö©
+                                </td>
+                                <td>
+            <textarea style='font-size:9pt;border:1px solid'name='content'
+                      style=background-image:url('img/bbs_text_line.gif');
+                      cols=74 rows=12 wrap=virtual><?php echo $content ?>
              </textarea></td>
-        </tr>
-        <tr> 
-          <td width=70 height=25 align=left>&nbsp;&nbsp;√∑∫Œ∆ƒ¿œ</td>
-          <td><input style='font-size:8pt;border:1px solid' type='file' 
-                   name='upfile' size=50 maxlength=100></td>
-        </tr>
-        <tr height=10> <td colspan=2></td> </tr>
-        <tr height=1 bgcolor=#5AB2C8><td colspan=2></td></tr>
-        <tr>
-          <td colspan=2 height=10 align=center valign=top 
-              bgcolor='FFFFFF'><br>
+                            </tr>
+                            <tr>
+                                <td width=70 height=25 align=left>&nbsp;&nbsp;Ï≤®Î∂ÄÌååÏùº</td>
+                                <td><input style='font-size:8pt;border:1px solid' type='file'
+                                           name='upfile' size=50 maxlength=100></td>
+                            </tr>
+                            <tr height=10> <td colspan=2></td> </tr>
+                            <tr height=1 bgcolor=#5AB2C8><td colspan=2></td></tr>
+                            <tr>
+                                <td colspan=2 height=10 align=center valign=top
+                                    bgcolor='FFFFFF'><br>
 
-           <input type=image src='img/i_ok.gif' 
-                 align=absmiddle border=0> &nbsp;<a href='list.php'>
-             <img style='cursor:hand'src='img/i_list.gif' 
-                  align=absmiddle border=0 ></a></td>
-        </tr>
-      </table>
-          </td>
-        </tr> 
-    </table>
-         </td>
-       </tr>
-    </form>
-  </table>
+                                    <input type=image src='img/i_ok.gif'
+                                           align=absmiddle border=0> &nbsp;<a href='list.php'>
+                                        <img style='cursor:hand'src='img/i_list.gif'
+                                             align=absmiddle border=0 ></a></td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+        </table>
+</form>
+</table>
 </body>
 </html>
