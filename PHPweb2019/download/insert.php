f7bde9e002b94990ce399bbd09854b2c53b8fa2d
tear94fall <tear94fall@naver.com>
2019-03-28 23:06:04 +0900

    insert부분 로직 계속 수정중....

--- PHPweb2019/download/insert.php
+++ PHPweb2019/download/insert.php
@@ -1,148 +1,128 @@
-<?
-   session_start();
+<?php
+session_start();
+$userid = $_SESSION['userid'];
+$username = $_SESSION['username'];
 
-   if(!$userid) {
-      echo("
-	   <script>
-	     window.alert('로그인 후 사용하세요.')
-	     history.go(-1)
-	   </script>
-	   ");	
-      exit;
-   }
-   
-   if(!$subject) {
-      echo("
-	   <script>
-	     window.alert('제목을 입력하세요.')
-	     history.go(-1)
-	   </script>
-	   ");
-       exit;
-   }
-   
-   if(!$content) {
-      echo("
-	   <script>
-	     window.alert('내용을 입력하세요.')
-	     history.go(-1)
-	   </script>
-	   ");
-      exit;
-   }
-   
-   include "../dbconn.php";
+$subject = $_REQUEST['content'];
+$content = $_REQUEST['subject'];
 
-   $regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
-   $ip = $REMOTE_ADDR;         // 방문자의 IP 주소를 저장
+$upfile = $_FILES['upfile'];
+
+$upfile_name = $upfile['name'];     //유효
+$upfile_size = $upfile['size'];         //유효
+$tmp_file = $upfile['tmp_name'];
 
 
-   if($upfile_name) 
-   {
-      if ( file_exists("data/$upfile_name") ){
-        echo("
-	   <script>
-	     window.alert('선택한 파일과 동일한 이름이 존재합니다.');
-	     history.go(-1)
-	   </script>
-	   ");
-         exit;
-      }
+if(!$userid) {
+    echo("<script>window.alert('로그인 후 사용하세요.'); history.go(-1)</script>");
+    exit;
+}
 
-      if( !$upfile) {
-         echo("
-	   <script>
-	     window.alert
-             ('업로드 파일 사이즈가 지정된 용량(2M)을 초과합니다.');
-	     history.go(-1)
-	   </script>
-           ");
-         exit;
-      }
+if(!$subject) {
+    echo("<script>window.alert('제목을 입력하세요.'); history.go(-1)</script>");
+    exit;
+}
 
-      if( strlen($upfile_size) < 7 ) {
-         $filesize = sprintf("%0.2f KB", $upfile_size/1000);
-      }
-      else
-      {
-         $filesize = sprintf("%0.2f MB", $upfile_size/1000000);
-      }
+if(!$content) {
+    echo("<script>window.alert('내용을 입력하세요.'); history.go(-1)</script>");
+    exit;
+}
 
-      if( !copy($upfile, "data/$upfile_name") )
-      {
-         echo("
-	   <script>
-	     window.alert
-             ('파일을 지정한 디렉토리에 복사하는데 실패했습니다.');
-	     history.go(-1)
-	   </script>
-           ");
-         exit;
-      }
-         
-      if ( !unlink($upfile) ) {
-         echo("
-	   <script>
-	     window.alert('임시파일을 삭제하는데 실패했습니다.');
-	     history.go(-1)
-	   </script>
-           ");
-         exit;
-      }
-   }
+include "../dbconn.php";
 
-   if (!$num) 
-   {
-      $depth = 0;   // depth, ord 를 0으로 초기화
-      $ord = 0;
+$regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
+$ip = $_SERVER["REMOTE_ADDR"];
 
-     // 레코드 삽입 명령
-      $sql = "insert into down_board
-              (depth, ord, id, name, subject,";
-      $sql .= "content, regist_day, hit, ip, filename, filesize) ";
-      $sql .= "values($depth, $ord, '$userid', '$username', 
+if($upfile_name)
+{
+    if ( file_exists("data/$file_path") ){
+        echo("<script>window.alert('선택한 파일과 동일한 이름이 존재합니다.');history.go(-1)</script>");
+        exit;
+    }
+
+    if( !$upfile) {
+        echo("<script>window.alert('업로드 파일 사이즈가 지정된 용량(2M)을 초과합니다.');history.go(-1)</script>");
+        exit;
+    }
+
+    if( strlen($upfile_size) < 7 ) {
+        $filesize = sprintf("%0.2f KB", $upfile_size/1000);
+    }
+    else
+    {
+        $filesize = sprintf("%0.2f MB", $upfile_size/1000000);
+    }
+
+    //move_uploaded_file($upfile_name, "data/$upfile_name")
+    //!copy($upfile, "data/$upfile_name");
+
+    if(!copy($upfile, "data/$upfile_name"))
+    {
+        echo("<script>window.alert('파일을 지정한 디렉토리에 복사하는데 실패했습니다.'); history.go(-1)</script>");
+        exit;
+    }
+    /*
+    if (!unlink($upfile)) {
+        echo("<script>window.alert('임시파일을 삭제하는데 실패했습니다.');history.go(-1)</script>");
+        exit;
+    }
+    */
+}
+
+$num = $_REQUEST['num'];
+$page = $_REQUEST['page'];
+
+
+if (!$num)
+{
+    $depth = 0;   // depth, ord 를 0으로 초기화
+    $ord = 0;
+
+    // 레코드 삽입 명령
+    $sql = "insert into down_board (depth, ord, id, name, subject,";
+    $sql .= "content, regist_day, hit, ip, filename, filesize) ";
+    $sql .= "values($depth, $ord, '$userid', '$username', 
                       '$subject',";
-      $sql .= "'$content', '$regist_day', 0, '$ip', '$upfile_name', 
-               '$filesize')";    
-      mysql_query($sql, $connect);  // $sql 에 저장된 명령 실행
+    $sql .= "'$content', '$regist_day', 0, '$ip', '$upfile_name', 
+               '$filesize')";
+    $result = $connect->query($sql) or die($this->_connect->error);
 
-      // 최근 auto_increment 필드(num) 값 가져오기
-      $sql = "select last_insert_id()"; 
-      $result = mysql_query($sql, $connect);
-      $row = mysql_fetch_array($result);
-      $auto_num = $row[0]; 
- 
-      // group_num 값 업데이트 
-      $sql = "update down_board set group_num = $auto_num 
-              where num=$auto_num";
-      mysql_query($sql, $connect);
-   }
-   else
-   {
-      // 부모 글 가져오기
-      $sql = "select * from down_board where num = $num";
-      $result = mysql_query($sql, $connect);
-      $row = mysql_fetch_array($result);
+    // 최근 auto_increment 필드(num) 값 가져오기
+    $sql = "select last_insert_id()";
+    $result = $connect->query($sql) or die($this->_connect->error);
+    $row = $result->fetch_array();
+    $auto_num = $row[0];
 
-      // 부모 글로 부터 group_num, depth, ord 값 설정
-      $group_num = $row[group_num];
-      $depth = $row[depth] + 1;
-      $ord = $row[ord] + 1;
+    // group_num 값 업데이트
+    $sql = "update down_board set group_num = $auto_num where num=$auto_num";
+    $result = $connect->query($sql) or die($this->_connect->error);
+}
+else
+{
+    // 부모 글 가져오기
+    $sql = "select * from down_board where num = $num";
+    $result = $connect->query($sql) or die($this->_connect->error);
+    $row = $result->fetch_array();
 
-      $sql = "update down_board set ord = ord + 1
+    // 부모 글로 부터 group_num, depth, ord 값 설정
+    $group_num = $row[group_num];
+    $depth = $row[depth] + 1;
+    $ord = $row[ord] + 1;
+
+    $sql = "update down_board set ord = ord + 1
              where group_num = $row[group_num] and ord > $row[ord]";
-      mysql_query($sql, $connect);  
+    $result = $connect->query($sql) or die($this->_connect->error);
 
-      $sql = "insert into down_board(group_num, depth, ord, id, name,
+    $sql = "insert into down_board(group_num, depth, ord, id, name,
               subject,";
-      $sql .= "content, regist_day, hit, ip, filename, filesize) ";
-      $sql .= "values($group_num, $depth, $ord, '$userid', '$username',
+    $sql .= "content, regist_day, hit, ip, filename, filesize) ";
+    $sql .= "values($group_num, $depth, $ord, '$userid', '$username',
                       '$subject',";
-      $sql .= "'$content', '$regist_day', 0, '$ip', '$upfile_name','$filesize')";
+    $sql .= "'$content', '$regist_day', 0, '$ip', '$upfile_name','$filesize')";
 
-      mysql_query($sql, $connect);  // $sql 에 저장된 명령 실행
-   }
-   mysql_close();                // DB 연결 끊기
-   
-   Header("Location:list.php?page=$page");  // list.php 로 이동합니다.
+    $result = $connect->query($sql) or die($this->_connect->error);
+}
+$connect->close();               // DB 연결 끊기
+
+Header("Location:list.php?page=$page");  // list.php 로 이동합니다.
 ?>
