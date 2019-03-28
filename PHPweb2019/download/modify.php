7a03e86b240c8aa6b97b934c9963504b635b169a
tear94fall <tear94fall@naver.com>
2019-03-29 01:41:08 +0900

    글 수정 정상작동

--- PHPweb2019/download/modify.php
+++ PHPweb2019/download/modify.php
@@ -1,54 +1,56 @@
-<?
-   session_start();
+<?php
+session_start();
 
-   include "../dbconn.php";
+$num = $_REQUEST['num'];
+$page = $_REQUEST['page'];
+$subject = $_REQUEST['subject'];
+$content = $_REQUEST['content'];
 
-   $sql = "select id from down_board where num = $num";
-   $result = mysql_query($sql, $connect);
-   $row = mysql_fetch_array($result);
+$userid = $_SESSION['userid'];
 
-   if($userid != "admin" and $userid != $row[id])   
-   // 비밀번호가 맞으면
-   {
-     echo("
-	   <script>
-             window.alert('수정 권한이 없습니다.')
-             history.go(-1)
-	   </script>
-          ");
-          exit;
-   }
+include "../dbconn.php";
 
-   if(!$subject) {
-     echo("
+$sql = "select id from down_board where num = $num";
+$result = $connect->query($sql) or die($this->_connect->error);
+$row = $result->fetch_array();
+
+
+if($userid != "admin" and $userid != $row['id'])
+    // 비밀번호가 맞으면
+{
+    //echo("<script>window.alert('수정 권한이 없습니다.');history.go(-1)</script>");
+    exit;
+}
+
+if(!$subject) {
+    echo("
 	   <script>
 	     window.alert('제목을 입력하세요.')
 	     history.go(-1)
 	   </script>
 	 ");
-	 exit;
-   }
-   
-   if(!$content) {
-     echo("
+    exit;
+}
+
+if(!$content) {
+    echo("
 	   <script>
 	     window.alert('내용을 입력하세요.')
 	     history.go(-1)
 	   </script>
 	 ");
-	 exit;
-   }
-   
-   $regist_day = date("Y-m-d (H:i)");  
-   // 현재의 '년-월-일-시-분'을 저장
-   $ip = $REMOTE_ADDR;         
-   // 방문자의 IP 주소를 저장
-   
-   $sql = "update down_board set subject='$subject', ";
-   $sql .= "content='$content' where num=$num";
-   
-   mysql_query($sql, $connect);
-   mysql_close();
-   
-   Header("Location:list.php?page=$page");  // list.php 로 이동
+    exit;
+}
+
+$regist_day = date("Y-m-d (H:i)");
+// 현재의 '년-월-일-시-분'을 저장
+$ip = $_SERVER["REMOTE_ADDR"];
+// 방문자의 IP 주소를 저장
+
+$sql = "update down_board set subject='$subject', ";
+$sql .= "content='$content' where num=$num";
+
+$result = $connect->query($sql) or die($this->_connect->error);
+
+Header("Location:list.php?page=$page");  // list.php 로 이동
 ?>
