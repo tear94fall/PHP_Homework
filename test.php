caacd0920acf82cf1308177375aad0740e655464
tear94fall <tear94fall@naver.com>
2019-03-31 19:05:36 +0900

    MySQL 연동 함수 부분 주석 변경

--- test.php
+++ test.php
@@ -13,7 +13,6 @@ $mysql_database = 'test';
 $mysql_port = '3306';
 $mysql_charset = 'utf8';
 
-
 //1. DB 연결
 $connect = new mysqli($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, $mysql_port);
 
@@ -35,9 +34,11 @@ $sql = "select * from students where number = '$number'";
 
 //4. 쿼리 실행
 $result = $connect->query($sql) or die($this->_connect->error);
+
+//5. 결과 처리 1
 $row = $result->fetch_array();
 
-//5. 결과 처리
+//5. 결과 처리 2
 while($row = $result->fetch_array())
 {
     echo $row['number']."<br>";
@@ -46,20 +47,15 @@ while($row = $result->fetch_array())
 //6. 연결 종료
 $connect->close();
 
-
 //post 방식으로 값 넘겨 받을때
 // $num = $_REQUEST['num'];
 
-
 // 세션에 값 등록
-//         $_SESSION['userid'] = $userid;
-//        $_SESSION['useername'] = $username;
-
-
+// $_SESSION['userid'] = $userid;
+// $_SESSION['useername'] = $username;
 
 // 세션값 변수에 등록
-//  $userid = $_SESSION['userid'];
-
+// $userid = $_SESSION['userid'];
 
 // 방문자의 IP 주소를 저장합니다.
 //$ip = $_SERVER["REMOTE_ADDR"];
